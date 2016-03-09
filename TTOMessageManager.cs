1c709f6306d7ba32179aabe6a3597f9067e9ea16
Ryan Stevenson <rystevenson93@googlemail.com>
2016-03-09 16:24:02 +0000

    Initial Scripts Commit

---
+++ TTOMessageManager.cs
@@ -0,0 +1,207 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TTOMessageManager : MonoBehaviour {
+
+
+    private static TTOMessageManager instance;
+    public static TTOMessageManager INSTANCE
+    {
+        get
+        {
+            if(instance == null)
+            {
+                GameObject TTOMessageManagerObj = new GameObject("TTOMessageManager");
+                instance = TTOMessageManagerObj.AddComponent<TTOMessageManager>();
+            }
+            return instance;
+        }
+    }
+	
+    public enum MessageType
+    {
+        SendIP,// = "SendIP",
+        SetLocalPlayerID,
+        RequestIP,// = "RequestIP",
+        MoveUnit,// = "MoveUnit",
+        Attack,// = "Attack",
+        ChangeHealth,// = "ChangeUnitHealth",
+        CreateUnit,// = "CreateUnit",
+        DestroyUnit,// = "DestroyUnit",
+        ChangeBuildingCapture,
+        CaptureBuilding,// = "CaptureBuilding"
+        ShowCube,
+		EndGame
+    }
+    
+    // Use this for initialization
+	void Start () {
+        if(instance != null)
+        {
+            DestroyImmediate(this);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+       
+	}
+
+    [SerializeField]
+    GameObject cube;
+
+    public void DecipherMessage(string message)
+    {
+        string[] messageArray = new string[10];
+        messageArray = message.Split(',');
+        Debug.Log("Message: "+messageArray[1]);
+        MessageType type = (MessageType)System.Enum.Parse(typeof(MessageType), messageArray[1]);
+        
+        switch (type)
+        {
+            case MessageType.SendIP:
+             //   TTONetworkManager.INSTANCE.Connect(messageArray[2]);
+			//TODO: Sort out seperate player numbers
+			     break;
+			case MessageType.SetLocalPlayerID:
+				TTOGameManager.INSTANCE.LocalPlayerID = System.Int32.Parse(messageArray[2]);
+			//TTONetworkManager.INSTANCE.networked = true;
+				break;
+            case MessageType.RequestIP:
+                CreateMessage(0, MessageType.SendIP, Network.player.ipAddress);
+                break;
+            case MessageType.MoveUnit:
+                //Message: PlayerNo, MessageType, unitID, Current Tile X, Current Tile Y, Desired Tile X, Desired Tile Y
+                int playerNumber = System.Int32.Parse(messageArray[0]);
+                //TTOUnit unit = TTOGameManager.INSTANCE.GetUnitByID(System.Int32.Parse(messageArray[2]), playerNumber);
+                TTOTile current = TTOGameManager.INSTANCE.GetTileByCoordinate(new Vector2(System.Int32.Parse(messageArray[3]),System.Int32.Parse(messageArray[4])));
+				TTOUnit unit = current.TileUnit.GetComponent<TTOUnit>();    
+				TTOTile desired = TTOGameManager.INSTANCE.GetTileByCoordinate(new Vector2(System.Int32.Parse(messageArray[5]), System.Int32.Parse(messageArray[6])));
+
+                TTOGameManager.INSTANCE.MultiplayerMovement(unit, current, desired);
+                break;
+            case MessageType.Attack:
+                //GameManager Attack Unit
+                break;
+            case MessageType.ChangeHealth:
+                //GameManager change units health
+			//Message: PlayerNo, MessageType UnitTileX, UnitTileY, AmountToChange
+				TTOUnit unitTemp = TTOGameManager.INSTANCE.GetTileByCoordinate(new Vector2(System.Int32.Parse(messageArray[2]), 
+				                                                                            System.Int32.Parse(messageArray[3]))).TileUnit.GetComponent<TTOUnit>();
+				unitTemp.ChangeHealth(System.Int32.Parse(messageArray[4]));
+			//TTOGameManager.INSTANCE.c
+			break;
+            case MessageType.CreateUnit:
+                //GameManager Create Unit
+                //Message: PlayerNo, MessageType, unitType, unitID, DesiredTileX, DesiredTileY
+			Debug.Log (messageArray[0] + " " + messageArray[1] + " " + messageArray[2] + " " + messageArray[3] + " " + messageArray[4] + " " + messageArray[5]);
+                TTOGameManager.INSTANCE.CreateUnit(TTOUnitManager.Instance.GetTypeByString(messageArray[2]), System.Int32.Parse(messageArray[0]),
+                    TTOGameManager.INSTANCE.GetTileByCoordinate(new Vector2(System.Int32.Parse(messageArray[4]), 
+                        System.Int32.Parse(messageArray[5]))));
+                break;
+            case MessageType.DestroyUnit:
+                //Message: PlayerNo, MessageType, UnitCoordinateX, UnitCoordinateY
+                var destroyUnitTempTile = TTOGameManager.INSTANCE.GetTileByCoordinate(new Vector2(System.Int32.Parse(messageArray[2]), 
+                        System.Int32.Parse(messageArray[3])));
+                destroyUnitTempTile.TileUnit.GetComponent<TTOUnit>().DestroyUnit();
+                //GameManager Destroy Unit
+                break;
+            case MessageType.ChangeBuildingCapture:
+                //Message: PlayerNo, MessageType, TileX, TileY, value
+                var changeBuildingCaptureTempTile = TTOGameManager.INSTANCE.GetTileByCoordinate(new Vector2(System.Int32.Parse(messageArray[2]),
+                        System.Int32.Parse(messageArray[3])));
+                changeBuildingCaptureTempTile.GetComponent<TTOBuilding>().DecreaseCaptureValue(System.Int32.Parse(messageArray[4]), 
+                    System.Int32.Parse(messageArray[0]));
+                break;
+            case MessageType.CaptureBuilding:
+                break;
+            case MessageType.ShowCube:
+                if (cube.activeInHierarchy)
+                    cube.SetActive(false);
+                else
+                    cube.SetActive(true);
+                break;
+			case MessageType.EndGame:
+				//Message: PlayerNo, MessageType, PlayerVictory
+				TTOGameManager.INSTANCE.HitEndGame(System.Int32.Parse(messageArray[2]));
+				break;
+            default:
+                Debug.Log("Error");
+                break;
+        }
+
+    }
+
+    public void SendCubeMessage()
+    {
+        CreateMessage(0, MessageType.ShowCube);
+    }
+
+    bool CheckForIP(string s)
+    {
+        if (s.Contains("ipAddress"))
+        {
+            Debug.Log("true");
+            return true;
+        }
+        return false;
+    }
+
+    //TODO: Change GameObjects to TTOUnits once Merge has occured
+    public void CreateMessage(int playerNumber, MessageType type, string neededString = "", GameObject unit1 = null, 
+        GameObject unit2 = null, int value = 0, TTOTile tile1 = null, TTOTile tile2 = null)
+    {
+        string messageString = "";
+        switch (type)
+        {
+            case MessageType.SendIP:
+                messageString = playerNumber.ToString()+ "," + MessageType.SendIP.ToString()+","+ neededString;
+                break;
+			case MessageType.SetLocalPlayerID:
+			Debug.Log ("Set Local Player ID");
+				messageString = playerNumber.ToString()+"," + MessageType.SetLocalPlayerID.ToString()+","+neededString;
+			break;
+            case MessageType.RequestIP:
+                messageString = playerNumber.ToString() + "," + MessageType.RequestIP.ToString();
+                break;
+            case MessageType.MoveUnit:
+                //Message: PlayerNo, MessageType, unitID, Current Tile X, Current Tile Y, Desired Tile X, Desired Tile Y
+                messageString = playerNumber.ToString() + "," + MessageType.MoveUnit.ToString() + "," + neededString + "," + tile1.TileCoordinates.x.ToString()
+                    + "," + tile1.TileCoordinates.y.ToString() + "," + tile2.TileCoordinates.x.ToString()
+                    + "," + tile2.TileCoordinates.y.ToString();
+                break;
+            case MessageType.Attack:
+                break;
+            case MessageType.ChangeHealth:
+			//Message: PlayerNo, MessageType UnitTileX, UnitTileY, AmountToChange
+			messageString = playerNumber.ToString() +","+ MessageType.ChangeHealth.ToString()+","+tile1.TileCoordinates.x + ","+tile1.TileCoordinates.y +","+value;
+                break;
+            case MessageType.CreateUnit:
+                //Message: PlayerNo, MessageType, unitType, unitID, DesiredTileX, DesiredTileY
+                messageString = playerNumber.ToString() + "," + MessageType.CreateUnit.ToString() + "," + neededString + "," + 0 + "," + tile1.TileCoordinates.x.ToString()
+                    + "," + tile1.TileCoordinates.y.ToString();
+                break;
+            case MessageType.DestroyUnit:
+                break;
+            case MessageType.CaptureBuilding:
+                break;
+            case MessageType.ShowCube:
+                messageString = playerNumber.ToString() + "," + MessageType.ShowCube.ToString();
+                break;
+			
+			case MessageType.ChangeBuildingCapture:
+			//Message: PlayerNo, MessageType, TileX, TileY, value
+				messageString = playerNumber.ToString() + "," +MessageType.ChangeBuildingCapture.ToString() + "," + tile1.TileCoordinates.x.ToString() 
+				+ "," + tile1.TileCoordinates.y.ToString() + "," + value;
+					
+			break;
+			case MessageType.EndGame:
+				messageString = playerNumber.ToString() +","+MessageType.EndGame.ToString()+","+value;
+			break;
+            default:
+                break;
+        }
+        //TTONetworkManager.INSTANCE.SendSocketMessage(messageString);
+    }
+}
---
+++ TTONetworkManager.cs
@@ -0,0 +1,188 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+public class TTONetworkManager : MonoBehaviour
+{
+    public GameObject checker;
+    int myReliableChannelId;
+
+    int socketId, socketPort = 8888, connectionId;
+    [SerializeField]
+    Text ipText, errorOutput;
+    [SerializeField]
+    InputField ipInput;
+
+    [SerializeField]
+	bool isHost = false;
+		public bool networked = false;
+
+    #region Instance
+    private static TTONetworkManager instance;
+    public static TTONetworkManager INSTANCE
+    {
+        get
+        {
+            if(instance == null)
+            {
+                GameObject TTONetworkManagerObj = new GameObject("TTONetworkManager");
+                instance = TTONetworkManagerObj.AddComponent<TTONetworkManager>();
+            }
+            return instance;
+        }
+    }
+    #endregion
+    void Start()
+    {
+         if(instance != null)
+         {
+             DestroyImmediate(this.gameObject);
+         }
+         else
+         {
+             instance = this;
+             DontDestroyOnLoad(this.gameObject);
+         }
+
+
+         InitializeNetwork();
+
+    }
+
+    public void InitializeNetwork()
+    {
+        NetworkTransport.Init();
+        ipText.text = Network.player.ipAddress;
+        ConnectionConfig conConf = new ConnectionConfig();
+
+        myReliableChannelId = conConf.AddChannel(QosType.Reliable);
+
+        int maxConnections = 4;
+        HostTopology topology = new HostTopology(conConf, maxConnections);
+
+        socketId = NetworkTransport.AddHost(topology, socketPort);
+        Debug.Log("Socket Open. SocketId is: " + socketId);
+    }
+
+    public void Connect()
+    {
+        byte error;
+        string ip = ipInput.text;
+        Debug.Log("IP Text Input: " + ip);
+        connectionId = NetworkTransport.Connect(socketId, ip, socketPort, 0, out error);
+        if(error != (byte)NetworkError.Ok)
+        {
+            
+        }
+        Debug.Log("ConnectionId: " + connectionId);
+    }
+
+    public void Connect(string IP)
+    {
+        byte error;
+        connectionId = NetworkTransport.Connect(socketId, IP, socketPort, 0, out error);
+        Debug.Log("ConnectionId: " + connectionId);
+    }
+   
+    public void ConnectAsHost()
+    {
+
+        //Being host just needs to be set to true, you don't need to connect to yourself as host. Just connect to others when they connect to you.
+        isHost = true;
+      
+		networked = true;
+        
+        //  byte error;
+      //  string ipAddress = Network.player.ipAddress;
+      ////  connectionId = NetworkTransport.Connect(socketId, ipAddress, socketPort, 0, out error);
+        
+    }
+
+    public void SendSocketMessage()
+    {
+        byte error;
+        byte[] buffer = new byte[1024];
+        Stream stream = new MemoryStream(buffer);
+        BinaryFormatter formatter = new BinaryFormatter();
+        formatter.Serialize(stream, "MessageSending");
+
+        int bufferSize = 1024;
+
+        NetworkTransport.Send(socketId, connectionId, myReliableChannelId, buffer, bufferSize, out error);
+    }
+
+    public void SendSocketMessage(string message)
+    {
+        byte error;
+        byte[] buffer = new byte[1024];
+        Stream stream = new MemoryStream(buffer);
+        BinaryFormatter formatter = new BinaryFormatter();
+        formatter.Serialize(stream, message);
+
+        int bufferSize = 1024;
+
+        NetworkTransport.Send(socketId, connectionId, myReliableChannelId, buffer, bufferSize, out error);
+    }
+
+	public void SendSetLocalPlayerIDMessage()
+	{
+		TTOMessageManager.INSTANCE.CreateMessage(TTOGameManager.INSTANCE.LocalPlayerID, TTOMessageManager.MessageType.SetLocalPlayerID, "1",null,null,0,null,null);
+
+	}
+    bool gotip = false;
+    void Update()
+    {
+        int recHostId;
+        int recConnectionId;
+        int recChannelId;
+        byte[] recBuffer = new byte[1024];
+        int bufferSize = 1024;
+        int dataSize;
+        byte error;
+        NetworkEventType recNetworkEvent = NetworkTransport.Receive(out recHostId, out recConnectionId, out recChannelId, recBuffer, bufferSize, out dataSize, out error);
+        //Debug.Log("recHostId:"+recHostId+ " recConID:"+recConnectionId+" recChanID"+recChannelId);
+        switch (recNetworkEvent)
+        {
+            case NetworkEventType.Nothing:
+                break;
+            case NetworkEventType.ConnectEvent:
+                Debug.Log("incoming connection event received");
+                Debug.Log("recHostId:" + recHostId + " recConID:" + recConnectionId + " recChanID" + recChannelId);
+                Debug.Log("ConID: " + connectionId);
+                if (isHost)
+                {
+                    if (connectionId != recConnectionId)
+                    {
+                        Debug.Log("In Here");
+                        // TTOMessageManager.INSTANCE.CreateMessage(1, TTOMessageManager.MessageType.RequestIP);
+                    }
+                }
+                else
+                {
+                    if (!gotip)
+                    {
+                      //  TTOMessageManager.INSTANCE.CreateMessage(0, TTOMessageManager.MessageType.SendIP, Network.player.ipAddress);
+                        gotip = true;
+                    }
+                }
+                break;
+            case NetworkEventType.DataEvent:
+                Stream stream = new MemoryStream(recBuffer);
+                BinaryFormatter formatter = new BinaryFormatter();
+                string message = formatter.Deserialize(stream) as string;
+                Debug.Log("incoming message event received: " + message);
+//                TTOMessageManager.INSTANCE.DecipherMessage(message);
+                
+                break;
+            case NetworkEventType.DisconnectEvent:
+                Debug.Log("remote client event disconnected");
+                break;
+        }
+
+    }
+
+    
+}
